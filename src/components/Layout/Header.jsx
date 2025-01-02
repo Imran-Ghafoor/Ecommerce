eb50de28a2921035c0da9c2a345c760e12effdd0
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-02 16:34:03 +0500

    added navbar and header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,30 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import NavBar from "./NavBar";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <MainHeader>
+      <NavLink to={"/"}>
+        <img src="./images/logo.png" alt="logo image" />
+      </NavLink>
+      <NavBar />
+    </MainHeader>
+  );
 };
+
+const MainHeader = styled.header`
+  padding: 0.4rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .logo {
+    height: 5rem;
+  }
+`;
 
 export default Header;
