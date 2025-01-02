37afa684a78d667e2867d94f63bca37007738402
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-02 16:53:09 +0500

    added home/heroSection/navbar

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return <h1>Footer</h1>;
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import HeroSection from "../components/UI/HeroSection";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const data = {
+    name: "Imran Store",
+  };
+  return <HeroSection myData={data} />;
 };
 
 export default Home;
