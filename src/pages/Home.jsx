49262371eb2cf0d5b2a386338a8e932421f1ffca
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-02 17:33:03 +0500

    updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import HeroSection from "../components/UI/HeroSection";
+import Services from "../components/UI/Services";
+import Trusted from "../components/UI/Trusted";
 
 const Home = () => {
   const data = {
     name: "Imran Store",
   };
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
 
 export default Home;
