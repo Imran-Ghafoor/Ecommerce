0f79adef6f415efa828d5aa976835daadf2a0659
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-06 20:19:05 +0500

    added main products page section

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,48 @@
+import styled from "styled-components";
+import FilterSection from "../components/UI/FilterSection";
+import ProductList from "../components/UI/ProductList";
+import Sort from "../components/UI/Sort";
+import { useFilterContext } from "../context/FilterContext";
+
 const Products = () => {
-  return <h1>Products</h1>;
+  const { filter_products } = useFilterContext();
+  console.log(filter_products);
+
+  return (
+    <Wrapper>
+      {/* first column */}
+      <div className="container grid grid-filter-column">
+        <div>
+          <FilterSection />
+        </div>
+
+        {/* second column */}
+        {/* first row */}
+        <section className="product-view-sort">
+          <div className="sort-filter">
+            <Sort />
+          </div>
+
+          {/* second row */}
+          <div className="main-product">
+            <ProductList />
+          </div>
+        </section>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  .grid-filter-column {
+    grid-template-columns: 0.2fr 1fr;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid-filter-column {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
 
 export default Products;
