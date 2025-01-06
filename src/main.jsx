0bfce5781c39a3f9f3fac04e6c670da673666bc0
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-06 20:17:49 +0500

    added filter context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+
 import App from "./App.jsx";
 import { AppProvider } from "./context/ProductContext.jsx";
+import { FilterContextProvider } from "./context/FilterContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <FilterContextProvider>
+        <App />
+      </FilterContextProvider>
     </AppProvider>
   </StrictMode>
 );
