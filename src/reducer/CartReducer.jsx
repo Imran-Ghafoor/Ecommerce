27c2232e1f043aea07e313b4e2441b8fd1c0585e
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-15 16:14:24 +0500

    added reducer action total order_amount

--- src/reducer/CartReducer.jsx
+++ src/reducer/CartReducer.jsx
@@ -116,6 +116,22 @@ const CartReducer = (state, action) => {
     };
   }
 
+  //add subtotal order-amount
+  if (action.type === "CART_TOTAL_PRICE") {
+    let total_price = state.cart.reduce((initialVal, curElem) => {
+      let { price, amount } = curElem;
+
+      initialVal = initialVal + price * amount;
+
+      return initialVal;
+    }, 0);
+
+    return {
+      ...state,
+      total_price,
+    };
+  }
+
   return state;
 };
 export default CartReducer;
