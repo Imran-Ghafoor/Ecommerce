778275d94df21877f5ebc3a847937b292f5be2df
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-15 18:53:00 +0500

    added auth0 provider to our App

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,24 @@ import App from "./App.jsx";
 import { AppProvider } from "./context/ProductContext.jsx";
 import { FilterContextProvider } from "./context/FilterContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppProvider>
-      <FilterContextProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterContextProvider>
-    </AppProvider>
+    <Auth0Provider
+      domain="dev-2vzushsqde476sgb.us.auth0.com"
+      clientId="1FoYf1NngY7maTdJFqIgkjcPN5RLuYVu"
+      authorizationParams={{
+        redirectUri: window.location.origin,
+      }}
+    >
+      <AppProvider>
+        <FilterContextProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterContextProvider>
+      </AppProvider>
+    </Auth0Provider>
   </StrictMode>
 );
