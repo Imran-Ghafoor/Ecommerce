778a8d36d844c3eb0418603400b4167d1fa950df
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-15 18:56:47 +0500

    added auth0 authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App.jsx";
 import { AppProvider } from "./context/ProductContext.jsx";
 import { FilterContextProvider } from "./context/FilterContext.jsx";
