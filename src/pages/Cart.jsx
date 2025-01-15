e1cbf145527189299eb36e19d0b9a88903b3492a
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2025-01-15 19:17:31 +0500

    added auth.. show user Info with auth0

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,9 +4,12 @@ import CartItem from "../components/UI/CartItem";
 import { NavLink } from "react-router-dom";
 import { Button } from "../components/UI/styles/Button";
 import FormatPrice from "../helpers/FormatPrice";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Cart = () => {
   const { cart, clearCart, total_price, shipping_fee } = useCartContext();
+
+  const { isAuthenticated, user } = useAuth0();
 
   if (cart.length === 0) {
     return (
@@ -18,6 +21,14 @@ const Cart = () => {
   return (
     <Wrapper>
       <div className="container">
+
+        {/* authenticate with auth0 */}
+        {isAuthenticated && (
+          <div className="cart-user--profile">
+            <img src={user.profile} alt={user.name} />
+            <h2 className="cart-user--name">{user.name}</h2>
+          </div>
+        )}
         <div className="cart_heading grid grid-five-column">
           <p>Item</p>
           <p className="cart-hide">Price</p>
